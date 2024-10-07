de7578a067fce42cfb3f91e401a447bc34ed3ffa
venatrix-osns <sadjith@venatrix.in>
2024-10-07 20:49:07 +0530

    Update add_column.sql

--- sql/add_column.sql
+++ sql/add_column.sql
@@ -1,3 +1,13 @@
-ALTER TABLE example_table
-ADD COLUMN age INT;
-);
+
+        SET @column_exists := (
+            SELECT COUNT(*)
+            FROM information_schema.columns
+            WHERE table_name = 'example_table'
+            AND table_schema = 'sample_db'
+            AND column_name = 'age'
+        );
+
+        -- Only add the column if it does not exist
+        IF @column_exists = 0 THEN
+            ALTER TABLE example_table ADD COLUMN age INT;
+        END IF;
